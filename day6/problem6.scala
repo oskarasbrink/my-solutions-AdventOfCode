c9df65ae150ec68596f1d7808527bd9c89437ebc
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-12-16 17:02:11 +0100

    problem 6, only scala after this one

--- day6/problem6.scala
+++ day6/problem6.scala
@@ -2,40 +2,28 @@ import scala.io.Source
 import scala.collection.mutable.ListBuffer
 import scala.util.control.Breaks._
 
-
-
 @main def main() = {
 
     val filename: String = "day6Input.txt"
     val lines: String = scala.io.Source.fromFile(filename).mkString
     breakable{
     for(n <- 4 to lines.length-1) {
-        
-        //var flatmap = linemap.flatten
         if(lines.substring(n-4,n).toCharArray.distinct.length>3){
             println(lines.substring(n-4,n))
             println(n)
             break
         } 
-        
-        
-        
 
     }
     }
     //part two
     breakable{
     for(n <- 14 to lines.length-1) {
-        
-        //var flatmap = linemap.flatten
         if(lines.substring(n-14,n).toCharArray.distinct.length>13){
             println(lines.substring(n-14,n))
             println(n)
             break
         } 
-        
-        
-        
 
     }
     }
